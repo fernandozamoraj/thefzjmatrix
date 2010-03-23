105c45e5ee0720c914aa741b49420082013ed2fa
Fernando.Zamora.Jimenez <Fernando.Zamora.Jimenez@aa0f5d5a-a6cd-db6a-dcad-8a05acbbac3e>
2010-03-23 19:09:19 +0000

    checked missing file

---
+++ TheMatrix/IsolatedStorage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TheMatrix
+{
+    public class IsolatedStorage
+    {
+        public void WriteAppSettings(string filename,
+                           Settings settings)
+        {
+            try
+            {
+                IsolatedStorageFile isf =
+                    System.IO.IsolatedStorage.IsolatedStorageFile.
+                           GetStore(
+                    IsolatedStorageScope.User |
+                    IsolatedStorageScope.Assembly, null, null);
+                Stream writer =
+                    new IsolatedStorageFileStream(filename,
+                                                  FileMode.Create, isf);
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(writer, settings);
+                writer.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Write Settings failed.", ex);
+            }
+        }
+
+        public Settings ReadSettings(string filename)
+        {
+            try
+            {
+                IsolatedStorageFile isf =
+                    System.IO.IsolatedStorage.IsolatedStorageFile.
+                           GetStore(
+                    IsolatedStorageScope.User |
+                    IsolatedStorageScope.Assembly, null, null);
+                Stream reader =
+                    new IsolatedStorageFileStream(filename, FileMode.
+                                                  Open, isf);
+                IFormatter formatter = new BinaryFormatter();
+                Settings settings = (Settings)formatter.
+                                         Deserialize(reader);
+                reader.Close();
+
+                return settings;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("ReadSettings failed", ex);
+            }
+        }
+    }
+}
